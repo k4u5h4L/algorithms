31e8afcb413b40a8516016abc4fb8d870a12b84e
Kaushal <kaushal.v.bhat@gmail.com>
2021-07-19 15:25:50 +0530

    Update Intersection_of_Two_Linked_Lists.py

--- leetcode/Intersection_of_Two_Linked_Lists.py
+++ leetcode/Intersection_of_Two_Linked_Lists.py
@@ -14,3 +14,27 @@ It is guaranteed that there are no cycles anywhere in the entire linked structur
 
 Note that the linked lists must retain their original structure after the function returns.
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        cur_a = headA
+        cur_b = headB
+        
+        while cur_a != cur_b:
+            if cur_a == None:
+                cur_a = headB
+            else:
+                cur_a = cur_a.next
+                
+            if cur_b == None:
+                cur_b = headA
+            else:
+                cur_b = cur_b.next
+        
+        return cur_a
