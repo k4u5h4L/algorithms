3f8cad9ee169fc5ddc3cf52583d2cc63e13b6513
Kaushal <kaushal.v.bhat@gmail.com>
2021-07-19 15:25:32 +0530

    Create Intersection_of_Two_Linked_Lists.py

---
+++ leetcode/Intersection_of_Two_Linked_Lists.py
@@ -0,0 +1,16 @@
+'''
+Intersection of Two Linked Lists
+Easy
+
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+a1 -> a2 -------↓
+                -> c1 -> c2 -> c3
+b1 -> b2 -> b3 -↑
+
+It is guaranteed that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+'''
