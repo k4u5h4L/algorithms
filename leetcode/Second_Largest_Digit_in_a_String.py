0467656f2cb71d0b13095d847dba7125e961e4ea
Kaushal <kaushal.v.bhat@gmail.com>
2021-07-22 10:53:51 +0530

    Create Second_Largest_Digit_in_a_String.py

---
+++ leetcode/Second_Largest_Digit_in_a_String.py
@@ -0,0 +1,43 @@
+'''
+Second Largest Digit in a String
+Easy
+
+Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
+
+An alphanumeric string is a string consisting of lowercase English letters and digits.
+
+ 
+
+Example 1:
+
+Input: s = "dfa12321afd"
+Output: 2
+Explanation: The digits that appear in s are [1, 2, 3]. The second largest digit is 2.
+
+Example 2:
+
+Input: s = "abc1111"
+Output: -1
+Explanation: The digits that appear in s are [1]. There is no second largest digit. 
+'''
+
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        max_num = 0
+        
+        for char in s:
+            try:
+                max_num = max(max_num, int(char))
+            except ValueError:
+                pass
+            
+        s = s.replace(str(max_num), "")
+        max_num = -1
+        
+        for char in s:
+            try:
+                max_num = max(max_num, int(char))
+            except ValueError:
+                pass
+            
+        return max_num
