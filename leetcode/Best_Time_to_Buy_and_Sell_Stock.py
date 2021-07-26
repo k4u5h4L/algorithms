5508f8127557e5ca6a40261b7236811bf83bf4f7
Kaushal <kaushal.v.bhat@gmail.com>
2021-07-26 13:56:33 +0530

    Update Best_Time_to_Buy_and_Sell_Stock.py

--- leetcode/Best_Time_to_Buy_and_Sell_Stock.py
+++ leetcode/Best_Time_to_Buy_and_Sell_Stock.py
@@ -15,5 +15,5 @@ class Solution:
         min_price = prices[0]
         for price in prices:
             min_price = min(price, min_price)
-            max_profit = max(max_profiprice - min_price)
+            max_profit = max(max_profit, price - min_price)
         return max_profit
