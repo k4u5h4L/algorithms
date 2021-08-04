b008259aa3f742f638ebd5ed7a82853eda7e4ebe
Kaushal <kaushal.v.bhat@gmail.com>
2021-08-04 15:03:55 +0530

    Create Last_Stone_Weight.py

---
+++ leetcode/Last_Stone_Weight.py
@@ -0,0 +1,48 @@
+'''
+Last Stone Weight
+Easy
+
+We have a collection of stones, each stone has a positive integer weight.
+
+Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+    If x == y, both stones are totally destroyed;
+    If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+ 
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone
+'''
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        if len(stones) == 1:
+            return stones[0]
+        
+        stones.sort(reverse=True)
+        
+        while len(stones) >= 2:
+            stone1 = stones.pop(0)
+            stone2 = stones.pop(0)
+            
+            if stone1 == stone2:
+                continue
+            else:
+                stones.append(abs(stone1 - stone2))
+                stones.sort(reverse=True)
+                
+        if len(stones) == 1:
+            return stones[0]
+        else:
+            return 0
