6ad8c6577c0933859e77e635b64e2c9dcbe0c112
Kaushal <kaushal.v.bhat@gmail.com>
2021-08-08 21:12:15 +0530

    Create Jewels_and_Stones.py

---
+++ leetcode/Jewels_and_Stones.py
@@ -0,0 +1,33 @@
+'''
+Jewels and Stones
+Easy
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+'''
+
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        hs = set(list(jewels))
+        
+        res = 0
+        
+        for char in stones:
+            if char in hs:
+                res += 1
+                
+        return res
