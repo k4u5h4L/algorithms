9a8f01b7ef65b67e1269c4e4d8706b9481d99efb
Kaushal <kaushal.v.bhat@gmail.com>
2021-08-12 18:32:49 +0530

    Update anagram_count.py

--- hashedin/anagram_count.py
+++ hashedin/anagram_count.py
@@ -1,5 +1,6 @@
 from itertools import permutations
 
+# you can use permutations, or check each section
 def count_anagrams(s, t):
     s_perm = list(permutations([char for char in s]))
 
@@ -15,6 +16,31 @@ def count_anagrams(s, t):
             count += 1
 
     return count
+# ----------------------------------------------------------------------------------------------------------
+# check if string is anagram of each other
+def is_anagram(s, s1):
+    s = list(s)
+    s1 = list(s1)
+
+    s.sort()
+    s1.sort()
+
+    return s == s1
+
+# send each subsection to be checked
+def count_anagrams_2(s, t):
+    res = 0
+
+    for i in range(len(t) - len(s)+1):
+        print(t[i:i+len(s)])
+        if is_anagram(s, t[i:i+len(s)]):
+            res += 1
+
+
+    return res
+
+# ----------------------------------------------------------------------------------------------------------
+
 
 
 def main():
